f3b665d4bfd105ad10f9ee5c0ac845793f8f278e
Sargis Dudaklyan <sdudakly@uci.edu>
2015-04-20 10:47:56 -0700

    Initial Commit

---
+++ ICS53_Lab2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ICS53_Lab2
+//
+//  Created by Sargis on 4/20/15.
+//  Copyright (c) 2015 Sargis. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
